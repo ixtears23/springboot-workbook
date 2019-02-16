3fd806269932593360c9407b9909406694e01482
ixtears23 <ixtears23@icloud.com>
2019-02-16 12:57:54 +0900

    실전 스프링 부트 워크 북 + 그 외

---
+++ simple-web-app/src/main/java/com/apress/j2ee/SimpleServlet.java
@@ -0,0 +1,23 @@
+package com.apress.j2ee;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SimpleServlet extends HttpServlet {
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<h1>간단한 자바 웹 애플리케이션</h1>");
+		out.println("</body>");
+		out.println("</html>");
+		
+	}
+}
